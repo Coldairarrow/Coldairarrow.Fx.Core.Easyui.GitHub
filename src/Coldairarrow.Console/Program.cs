dd7034d5bdb3abad7d8530fec7a887bacca3631e
Coldairarrow <xgc15657519397@outlook.com>
2018-10-01 19:04:09 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -8,9 +8,6 @@ namespace Coldairarrow.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var dbHelper = DbHelperFactory.GetDbHelper(DatabaseType.MySql, "server=127.0.0.1;user id=root;password=root;persistsecurityinfo=True;database=coldairarrow.fx.net.easyui.github");
-            var list = dbHelper.GetDbTableInfo("base_user");
-            
 
             Console.WriteLine("Hello World!");
         }
