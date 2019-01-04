5a8dc4e2f25a7eb948287c838074fb1488a9da28
Coldairarrow <xgc15657519397@outlook.com>
2019-01-04 10:30:43 +0800

    更新

--- src/Coldairarrow.Web/App_Start/01Handler/登录校验/CheckLoginAttribute.cs
+++ src/Coldairarrow.Web/App_Start/01Handler/登录校验/CheckLoginAttribute.cs
@@ -11,7 +11,7 @@ namespace Coldairarrow.Web
     /// <summary>
     /// 校验登录
     /// </summary>
-    public class CheckLoginAttribute :Attribute, IActionFilter
+    public class CheckLoginAttribute : Attribute, IActionFilter
     {
         /// <summary>
         /// Action执行之前执行
@@ -19,31 +19,42 @@ namespace Coldairarrow.Web
         /// <param name="filterContext">过滤器上下文</param>
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            //若为本地测试，则不需要登录
-            if (GlobalSwitch.RunModel == RunModel.LocalTest)
+            try
             {
-                return;
-            }
-            //判断是否需要登录
-            List<string> attrList = FilterHelper.GetFilterList(filterContext);
-            bool needLogin = attrList.Contains(typeof(CheckLoginAttribute).FullName) && !attrList.Contains(typeof(IgnoreLoginAttribute).FullName);
+                //若为本地测试，则不需要登录
+                if (GlobalSwitch.RunModel == RunModel.LocalTest)
+                {
+                    return;
+                }
+                //判断是否需要登录
+                List<string> attrList = FilterHelper.GetFilterList(filterContext);
+                bool needLogin = attrList.Contains(typeof(CheckLoginAttribute).FullName) && !attrList.Contains(typeof(IgnoreLoginAttribute).FullName);
 
-            //转到登录
-            if (needLogin && !Operator.Logged())
+                //转到登录
+                if (needLogin && !Operator.Logged())
+                {
+                    RedirectToLogin();
+                }
+            }
+            catch (Exception ex)
+            {
+                BusHelper.HandleException(ex);
+                RedirectToLogin();
+            }
+
+            void RedirectToLogin()
             {
                 UrlHelper urlHelper = new UrlHelper(filterContext);
                 string loginUrl = urlHelper.Content("~/Home/Login");
-                string script =$@"    
+                string script = $@"    
 <html>
     <script>
         top.location.href = '{loginUrl}';
     </script>
 </html>
 ";
-                filterContext.Result = new ContentResult { Content = script,ContentType= "text/html;charset=utf-8" };
+                filterContext.Result = new ContentResult { Content = script, ContentType = "text/html" };
             }
-            else
-                return;
         }
 
         /// <summary>
@@ -52,7 +63,7 @@ namespace Coldairarrow.Web
         /// <param name="filterContext"></param>
         public void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            
+
         }
     }
 }
